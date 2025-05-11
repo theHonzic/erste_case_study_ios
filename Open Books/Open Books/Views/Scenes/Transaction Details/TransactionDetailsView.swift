69990aa616fae32a9c1575ce29ff5f2552477b9b
Jan Janovec <99410175+theHonzic@users.noreply.github.com>
2025-05-11 23:53:03 +0200

    Add GeneratePaymentView and TransactionDetailsView for payment and transaction display

---
+++ Open Books/Open Books/Views/Scenes/Transaction Details/TransactionDetailsView.swift
@@ -0,0 +1,15 @@
+//
+//  TransactionDetailsView.swift
+//  Open Books
+//
+//  Created by Jan Janovec on 11.05.2025.
+//
+
+import SwiftUI
+
+struct TransactionDetailsView: View {
+    var transaction: Transaction
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
